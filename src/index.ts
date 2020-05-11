ea186791976e3b897264bcb16f414fc82c649113
Vlady Veselinov <vladislav.veselinov@fanduel.com>
2020-05-11 23:56:25 +0100

    fix babel errors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`Hello, I'm a Cloudflare worker, you made a: ${request.method} request.`)
+  return new Response(`Hello, I'm Vlady's Cloudflare worker, you made a: ${request.method} request.`)
 }
 
 addEventListener('fetch', (event) => {
